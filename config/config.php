8251622057deca1023c596eaf734b887b42e93b6
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2025-06-17 14:43:29 -0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,18 +2,12 @@
 define('DB_SERVER', getenv('DB_SERVER'));
 define('DB_USERNAME', getenv('DB_USERNAME'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
-
-$DB_DATABASE = 'VICA-PROD';
-$HTTP_ORIGIN = $_SERVER['HTTP_ORIGIN'];
-if (preg_match('/dev/', $HTTP_ORIGIN) || preg_match('/sandbox/', $HTTP_ORIGIN) || preg_match('/localhost/', $HTTP_ORIGIN)) {
-    $DB_DATABASE = 'VICA-DEV';
-}
-define('DB_DATABASE', $DB_DATABASE);
+define('DB_NAME', getenv('DB_NAME'));
 
 function dbConnection() {
     $connection = null;
     try {
-        $connection = new PDO('sqlsrv:server=' . DB_SERVER . ';Database=' . DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+        $connection = new PDO('sqlsrv:server=' . DB_SERVER . ';Database=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $error) {
