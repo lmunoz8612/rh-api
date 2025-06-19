5adebb5aa6589a426ddf177c405b4bbe44b65f05
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2025-06-19 17:58:43 -0600

    Update routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -25,8 +25,8 @@ $body = json_decode(file_get_contents('php://input'), true);
 $main = $requestUriParts[0] ?? '';
 print_r($requestUriParts);
 $route = $requestUriParts[1] ?? null;
-die();
 $subroutes = array_slice($requestUriParts, 2);
+die();
 
 if (str_starts_with($route, 'login')) {
     login($method, $subroutes, $body);
