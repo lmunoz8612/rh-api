ccd67c6fc8b5be7551e1ec4e1b05a6e431c054a8
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2025-06-18 23:59:29 -0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -7,24 +7,14 @@ define('DB_NAME', getenv('DB_NAME'));
 function dbConnection() {
     $connection = null;
     try {
-        $connection = new PDO(
-            'sqlsrv:server=' . DB_SERVER . ';Database=' . DB_NAME,
-            DB_USERNAME,
-            DB_PASSWORD,
-            [
-                PDO::SQLSRV_ATTR_ENCRYPT => true,
-                PDO::SQLSRV_ATTR_TRUST_SERVER_CERTIFICATE => true,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            ]
-        );
-
+        $connection = new PDO('sqlsrv:server=' . DB_SERVER . ';Database=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
         http_response_code(200);
         echo json_encode(['ok' => true, 'message' => 'Conexión establecida'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    } catch (PDOException $error) {
+    }
+    catch (PDOException $error) {
         http_response_code(500);
         echo json_encode(['error' => true, 'message' => 'Error de conexión: ' . $error->getMessage()], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     }
-
     return $connection;
 }
 
