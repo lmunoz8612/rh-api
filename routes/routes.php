a83b475dbe99bcea836ee7a51e3e86566000fcfe
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2025-06-19 18:11:47 -0600

    Update routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -10,6 +10,7 @@ session_set_cookie_params([
 session_start();
 
 require_once '../utils/response.php';
+require_once '../controlers/tokenController.php';
 
 spl_autoload_register(function ($className) {
     $controllerPath = __DIR__ . '/../controllers/' . $className . '.php';
