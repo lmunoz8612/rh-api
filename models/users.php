fe4f7f69495ffe1315fab7861e3dacfdde61b5a0
lmunoz8612 <lvmunozf@outlook.com>
2025-06-21 00:55:36 -0600

    Updt: User Profile Picture

--- models/users.php
+++ models/users.php
@@ -22,7 +22,7 @@ class Users {
                     jpd.job_position_department,
                     jpo.job_position_office,
                     ua.username,
-                    uf.[file] AS profile_picture,
+                    CONCAT('data:image/', uf.[file_extension], ';base64,', uf.[file]) AS profile_picture,
                     ua.last_access_at
                 FROM [user].[users] u
                 LEFT JOIN [user].[users_auth] ua ON u.pk_user_id = ua.fk_user_id
