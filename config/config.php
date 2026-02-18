57c48eeea6ae8216ae3dc2aad3ea9f0367a209e0
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2026-02-17 18:24:43 -0600

    Change database connection from MySQL to SQL Server

--- config/config.php
+++ config/config.php
@@ -1,22 +1,20 @@
 <?php
 define('DB_SERVER', getenv('DB_SERVER'));
+define('DB_DATABASE', getenv('DB_NAME'));
 define('DB_USERNAME', getenv('DB_USERNAME'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_NAME', getenv('DB_NAME'));
 
 function dbConnection() {
     $connection = null;
     try {
-        $connection = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME . ';charset=utf8mb4', DB_USERNAME, DB_PASSWORD);
-        http_response_code(200);
-        json_encode(['ok' => true, 'message' => 'Conexión establecida'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $connection = new PDO('sqlsrv:server=' . DB_SERVER . ';Database=' . DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $error) {
         http_response_code(500);
         echo json_encode(['error' => true, 'message' => 'Error de conexión: ' . $error->getMessage()], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-        exit();
     }
-
+    
     return $connection;
 }
 ?>
