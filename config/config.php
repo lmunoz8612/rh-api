6d8f94320b4043dc947bb497810800fe5e2f81f0
lmunoz8612 <95001496+lmunoz8612@users.noreply.github.com>
2026-02-18 01:28:29 -0600

    Fix syntax error in JSON encoding for dbConnection

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 function dbConnection() {
     $connection = null;
-    echo json_encode(['ok' => 'Test', 'DB_SERVER' => DB_SERVER, 'Database' => DB_DATABASE, 'DB_USERNAME' => DB_USERNAME, 'DB_PASSWORD' => DB_PASSWORD];
+    echo json_encode(['ok' => 'Test', 'DB_SERVER' => DB_SERVER, 'Database' => DB_DATABASE, 'DB_USERNAME' => DB_USERNAME, 'DB_PASSWORD' => DB_PASSWORD]);
     try {
         $connection = new PDO('sqlsrv:server=' . DB_SERVER . ',1433;Database=' . DB_DATABASE, DB_USERNAME, DB_PASSWORD);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
