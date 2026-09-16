e975e9c5e40117469600bc62592542fdb40421db
Luis Vicente Muñoz Fonseca <luis.munozfon@laureate.mx>
2026-09-16 12:24:06 -0600

    ci: remove azure pipeline workflow

---
+++ config/config_mssql.php
@@ -0,0 +1,21 @@
+<?php
+define('DB_SERVER', getenv('DB_SERVER'));
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_NAME', getenv('DB_NAME'));
+
+function dbConnection() {
+    $connection = null;
+    try {
+        $connection = new PDO('sqlsrv:server=' . DB_SERVER . ';Database=' . DB_NAME . ';Encrypt=no', DB_USERNAME, DB_PASSWORD); // Retirar ;Encrypt=no
+        http_response_code(200);
+        json_encode(['ok' => true, 'message' => 'Conexión establecida'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    }
+    catch (PDOException $error) {
+        http_response_code(500);
+        echo json_encode(['error' => true, 'message' => 'Error de conexión: ' . $error->getMessage()], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        exit();
+    }
+    return $connection;
+}
+?>
